fb5a80f627018811675c1048d8838c3e2b2e38e6
Glaver <xeron777@gmail.com>
2020-12-09 17:08:50 +0700

    add model for movies

---
+++ MoviesToWatch/Model/MovieModel.swift
@@ -0,0 +1,44 @@
+//
+//  MovieModel.swift
+//  MoviesToWatch
+//
+//  Created by Vladyslav on 9/12/20.
+//https://developers.themoviedb.org/3/movies/get-latest-movie
+
+import Foundation
+
+public struct MovieDataDTO {
+    let page: Int
+    let totalResults: Int
+    let totalPages: Int
+    let results: [MovieModel]
+}
+
+struct MovieModel {
+    let popularity: Float
+    let id: Int
+    let title: String
+    let backdropPath: String?
+    let posterPath: String?
+    let overview: String
+    let releaseDate: Date
+    let voteAverage: Float
+    let voteCount: Int
+    let video: Bool
+    let adult: Bool
+    let originalLanguage: String
+    let originalTitle: String
+    let genreIds: [Int]
+}
+
+protocol MovieShowViewProtocol {
+    var popularity: Float { get }
+    var id: Int { get }
+    var title: String { get }
+    var backdropPath: String? { get }
+    var posterPath: String? { get }
+    var genreIds: [Int] { get }
+    var overview: String { get }
+    var releaseDate: Date { get }
+    var voteAverage: Float { get }
+}
